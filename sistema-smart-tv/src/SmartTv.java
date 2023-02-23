3d8d7438f4ef2f203838f671d67b5b049d2c7db5
ThyagooSr <thyagoosr@gmail.com>
2023-02-23 18:57:13 -0300

    adiçao de mudança de canal

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -4,6 +4,10 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
     public void aumentarCanal() {
         canal++;
     }
